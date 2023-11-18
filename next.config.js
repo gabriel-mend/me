07fde2eb5b6cc557b51e6ed047aebd198b9dcc39
gabriel-mend <crowofcode@gmail.com>
2023-11-18 10:28:06 -0300

    feat: configurando dominio das imagens

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.media.licdn.com',
+      },
+    ],
+    domains: [
+      "raw.githubusercontent.com",
+      "media.licdn.com"
+    ]
+  }
 }
 
 module.exports = nextConfig 
